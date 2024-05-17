81d54ab2450b254c6645fec2e2a7732aae088c09
mapabuena <160073779+mapabuena@users.noreply.github.com>
2024-05-17 01:30:38 -0300

    Update and rename 10g.js to 10h.js

--- 10h.js
+++ 10h.js
@@ -131,7 +131,6 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 });
-});
                                             
   
     const searchButton = document.getElementById('searchButton');
