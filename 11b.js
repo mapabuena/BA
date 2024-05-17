83a323d182f56d0819cc5dc34b7d1dd5b4302bac
mapabuena <160073779+mapabuena@users.noreply.github.com>
2024-05-17 18:20:48 -0300

    Update and rename 11a.js to 11b.js

--- 11b.js
+++ 11b.js
@@ -195,7 +195,7 @@ document.addEventListener('DOMContentLoaded', function() {
         if (startDateInput.value && endDateInput.value < startDateInput.value) {
             endDateInput.value = startDateInput.value; // Correct this line to maintain logical consistency
         }
-    });
+    }
 });
 
 
