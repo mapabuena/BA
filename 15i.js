32de5486e2fa67ba83d7665d6db8e703fb1d4614
mapabuena <160073779+mapabuena@users.noreply.github.com>
2024-05-26 02:06:45 -0300

    Update and rename 15h.js to 15i.js

--- 15i.js
+++ 15i.js
@@ -426,17 +426,6 @@ function convertRecurringToSpecificDates(schedule, startDate, endDate) {
         "Saturday": 6
     };
 
-function convertRecurringToSpecificDates(schedule, startDate, endDate) {
-    const dayMap = {
-        "Sunday": 0,
-        "Monday": 1,
-        "Tuesday": 2,
-        "Wednesday": 3,
-        "Thursday": 4,
-        "Friday": 5,
-        "Saturday": 6
-    };
-
     let specificDates = [];
     schedule.forEach(event => {
         const dayOfWeek = dayMap[event.day];
