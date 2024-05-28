5993685ec35098f313ea1e59b52220d1abccb8bd
mapabuena <160073779+mapabuena@users.noreply.github.com>
2024-05-28 02:46:00 -0300

    Update and rename 16q.js to 16r.js

--- 16r.js
+++ 16r.js
@@ -11,11 +11,6 @@ let activeFilters = {
     category: [],
 };
 
-let markers = [];
-let activeFilters = {
-    category: [],
-};
-
 const nightStyle = 'mapbox://styles/n31ld/clwo829pt03rh01ql4z379sp2';
 const originalStyle = 'mapbox://styles/n31ld/clwocpejw03s201ql6pto7fh9';
 
