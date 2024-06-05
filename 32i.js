f614700942ec2a8b0c3be1b820f896001afca0b8
mapabuena <160073779+mapabuena@users.noreply.github.com>
2024-06-05 04:00:51 -0300

    Update and rename 32h.js to 32i.js

--- 32i.js
+++ 32i.js
@@ -23,17 +23,20 @@ let selectedMarkerIndex = null; // Variable to keep track of the selected marker
 let directions;
 
 function initializeDirectionsControl() {
-    directions = new MapboxDirections({
-        accessToken: mapboxgl.accessToken,
-        unit: 'metric',
-        profile: 'mapbox/driving',
-        controls: {
-            inputs: true,
-            instructions: true,
-        }
-    });
+    if (!directions) {
+        directions = new MapboxDirections({
+            accessToken: mapboxgl.accessToken,
+            unit: 'metric',
+            profile: 'mapbox/driving',
+            controls: {
+                inputs: true,
+                instructions: true,
+            }
+        });
 
-    map.addControl(directions, 'top-left');
+        document.getElementById('directions-control').appendChild(directions.onAdd(map));
+    }
+}
 
     // Listen for route updates and check for layer existence
     directions.on('route', () => {
@@ -88,6 +91,8 @@ document.addEventListener('DOMContentLoaded', function() {
     setupFormHandlers();
     setupMapEvents();
     setupInfoItemHoverEffects();
+    setupDirectionsButton();
+     initializeDirectionsControl();
     // Initially add daymode-text class to h4 elements
     document.querySelectorAll('.info-item h4').forEach(h4 => {
         h4.classList.add('daymode-text');
