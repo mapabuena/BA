012cc446783a6910296869f545817a0237bcf748
mapabuena <160073779+mapabuena@users.noreply.github.com>
2024-06-14 18:14:16 -0300

    Update and rename 0-05.js to 0-04.js

--- 0-04.js
+++ 0-04.js
@@ -1,4 +1,4 @@
-fmapboxgl.accessToken = 'pk.eyJ1IjoibjMxbGQiLCJhIjoiY2x0NHc5NjVpMDdzaDJscGE5Y2gyYnQ5MyJ9.zfzXUlLbNlVbr9pt4naycw'; // Replace with your actual access token
+mapboxgl.accessToken = 'pk.eyJ1IjoibjMxbGQiLCJhIjoiY2x0NHc5NjVpMDdzaDJscGE5Y2gyYnQ5MyJ9.zfzXUlLbNlVbr9pt4naycw'; // Replace with your actual access token
 
 let map = new mapboxgl.Map({
     container: 'map',
